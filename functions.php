d3db3c53faf6867b9b35ea125bc24ba39f48973f
Marco Aspeitia <maspeitia.31@gmail.com>
2021-04-11 09:32:47 -0500

    Defining specific image size in functions.php

--- functions.php
+++ functions.php
@@ -2,9 +2,19 @@
 /* 
 *   Load theme's CSS and Scritps
 */
-function my_theme_enqueue_styles() {
+function stiles_divi_theme_enqueue_styles() {
+
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style'), wp_get_theme()->get('Version'));
     
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'stiles_divi_theme_enqueue_styles' );
+
+/* Functions that loads when the theme is activated */
+function stiles_divi_theme_setup(){
+
+    /* Defining images size */
+    add_image_size( 'gallery_slides_images', 800, 450, true );
+
+}
+add_action( 'after_setup_theme', 'stiles_divi_theme_setup' );
